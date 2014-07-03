09de197749200ea72c16e28a269ac3742e39ad92
Edwin Raj <Edwin.be@gmail.com>
2014-07-03 12:04:34 +0100

    Size and MaxDepth functionality added

--- Trees/BinaryTrees/BinarySearchTree.cs
+++ Trees/BinaryTrees/BinarySearchTree.cs
@@ -8,22 +8,30 @@ namespace Trees
 {
     public class BinarySearchTree<T> 
     {
-        private TreeNode<T> RootNode;
+        private TreeNode<T> _rootNode;
 
         public BinarySearchTree()
         {
 
         }
 
+        public TreeNode<T> RootNode
+        {
+            get
+            {
+                return _rootNode;
+            }
+        }
+
         public void Insert(T data)
         {
-            if (RootNode == null)
+            if (_rootNode == null)
             {
-                RootNode = new TreeNode<T>(data);
+                _rootNode = new TreeNode<T>(data);
             }
             else
             {
-                TreeNode<T> currentNode = RootNode;
+                TreeNode<T> currentNode = _rootNode;
                 Stack<TreeNode<T>> localStack = new Stack<TreeNode<T>>();
                 while (true)
                 {
@@ -53,6 +61,106 @@ namespace Trees
             }
         }
 
+        public bool Lookup(T data)
+        {
+            bool isValid = false;
+
+            if (_rootNode == null)
+                return isValid;
+
+            TreeNode<T> currentNode = _rootNode;
+
+            while (true)
+            {
+                if (IsEqual(data, currentNode.Value))
+                {
+                    isValid = true;
+                    break;
+                }
+                else
+                {
+                    if (IsLessThanOrEqualTo(data, currentNode.Value))
+                    {
+                        currentNode = currentNode.LeftNode;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.RightNode;
+                    }
+                    if (currentNode == null)
+                        break;
+                }
+            }
+
+
+            return isValid;
+        }
+
+        public void PrintTree()
+        {
+            Queue<T> valueQueue = new Queue<T>();
+            Queue<TreeNode<T>> nodeQueue = new Queue<TreeNode<T>>();
+
+            TreeNode<T> currentNode = _rootNode;
+            while (true)
+            {
+                valueQueue.Enqueue(currentNode.Value);
+                
+                if (currentNode.LeftNode != null)
+                {
+                    //valueQueue.Enqueue(currentNode.LeftNode.Value);
+                    nodeQueue.Enqueue(currentNode.LeftNode);
+                }
+
+                if (currentNode.RightNode != null)
+                {
+                    //valueQueue.Enqueue(currentNode.RightNode.Value);
+                    nodeQueue.Enqueue(currentNode.RightNode);
+                }
+
+                if (nodeQueue.Count == 0)
+                    break;
+
+                currentNode = nodeQueue.Dequeue();
+              
+            }
+            valueQueue.ToList().ForEach(x=>Console.Write(x));
+
+        }
+
+        public int Size(TreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return Size(node.LeftNode) + 1 + Size(node.RightNode);
+            }
+        }
+
+        public int MaxDepth(TreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int leftTreeDepth = MaxDepth(node.LeftNode);
+                int rightTreeDepth = MaxDepth(node.RightNode);
+                if (leftTreeDepth > rightTreeDepth)
+                {
+                    return leftTreeDepth + 1;
+                }
+                else
+                {
+                    return rightTreeDepth + 1;
+                }
+            }
+        }
+
         #region Privates
         private bool IsLessThanOrEqualTo<T>(T data,T currentNodeValue)
         {
@@ -64,11 +172,13 @@ namespace Trees
             return Comparer<T>.Default.Compare(data, currentNodeValue) > 0;
         }
 
+        private bool IsEqual<T>(T data, T currentNodeValue)
+        {
+            return Comparer<T>.Default.Compare(data, currentNodeValue) == 0;
+        }
+
         #endregion Privates
 
-        public void PrintTree()
-        {
- 
-        }
+      
     }
 }
--- Trees/TreeTest/BinaryTreeTest.cs
+++ Trees/TreeTest/BinaryTreeTest.cs
@@ -10,7 +10,50 @@ namespace TreeTest
     public class BinaryTreeTest
     {
         [TestMethod]
-        public void ConstructTree()
+        public void PrintTreeTest()
+        {
+            BinarySearchTree<int> bst = ConstructTree();
+            bst.PrintTree();
+
+        }
+
+        [TestMethod]
+        public void LookupTest()
+        {
+            BinarySearchTree<int> bst = ConstructTree();
+            Assert.IsTrue(bst.Lookup(5));
+            Assert.IsTrue(bst.Lookup(6));
+            Assert.IsFalse(bst.Lookup(0));
+        }
+
+
+        [TestMethod]
+        public void SizeTest()
+        {
+            BinarySearchTree<int> bst = ConstructTree(5,4,8);
+            Assert.AreEqual(3, bst.Size(bst.RootNode));
+
+            bst = ConstructTree(5);
+            Assert.AreEqual(1, bst.Size(bst.RootNode));
+
+            bst = new BinarySearchTree<int>();
+            Assert.AreEqual(0, bst.Size(bst.RootNode));
+        }
+
+        [TestMethod]
+        public void MaxDepthTest()
+        {
+            BinarySearchTree<int> bst = ConstructTree(5, 4, 8);
+            Assert.AreEqual(2, bst.MaxDepth(bst.RootNode));
+
+            bst = ConstructTree(5);
+            Assert.AreEqual(1, bst.MaxDepth(bst.RootNode));
+
+            bst = new BinarySearchTree<int>();
+            Assert.AreEqual(0, bst.MaxDepth(bst.RootNode));
+        }
+
+        private static BinarySearchTree<int> ConstructTree()
         {
             string fileName = "BinarySearchTreeBasic.txt";
             string[] numbers = ExtractContent(fileName);
@@ -19,8 +62,18 @@ namespace TreeTest
             {
                 bst.Insert(int.Parse(currentNumber));
             }
+            return bst;
+        }
 
-
+        private static BinarySearchTree<int> ConstructTree(params int[] values)
+        {
+            string[] numbers = values.Select(x => x.ToString()).ToArray();
+            BinarySearchTree<int> bst = new BinarySearchTree<int>();
+            foreach (string currentNumber in numbers)
+            {
+                bst.Insert(int.Parse(currentNumber));
+            }
+            return bst;
         }
 
         private static string[] ExtractContent(string fileName)
